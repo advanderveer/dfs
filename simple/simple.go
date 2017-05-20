050f88b78c7aad1aa0fbbb1b0c38193abcb831aa
advanderveer <advanderveer@gmail.com>
2017-05-20 12:14:52 +0200

    basic file write passthrough

--- simple/simple.go
+++ simple/simple.go
@@ -2,26 +2,40 @@ package simple
 
 import (
 	"fmt"
+	"os"
+
+	"path/filepath"
 
 	"github.com/billziss-gh/cgofuse/fuse"
 )
+
+//FileHandle is our in-memory handle for open files
+type FileHandle struct {
+	openCount int
+	*os.File
+}
 
 //FS is a basic file system
 type FS struct {
 	fuse.FileSystemBase
 	ReadyCh chan struct{}
+
+	dbdir   string
+	handles map[uint64]*FileHandle
 }
 
 //NewFS creates the filesystem
-func NewFS() *FS {
+func NewFS(dir string) *FS {
 	return &FS{
 		ReadyCh: make(chan struct{}),
+
+		dbdir:   dir,
+		handles: map[uint64]*FileHandle{},
 	}
 }
 
-const (
+var (
 	filename = "hello"
-	contents = "hello, world\n"
 )
 
 //Init is called when the file system is created.
@@ -29,50 +43,102 @@ func (fs *FS) Init() {
 	fs.ReadyCh <- struct{}{}
 }
 
-//Open a file
-func (fs *FS) Open(path string, flags int) (errc int, fh uint64) {
+//Open a file, returns a file discriptor that can be used for further interactions on the file.
+func (fs *FS) Open(path string, flags int) (errc int, fd uint64) {
 	switch path {
 	case "/" + filename:
-		return 0, 0
+		var err error
+		fh := &FileHandle{}
+		fh.File, err = os.OpenFile(filepath.Join(fs.dbdir, path), flags, 0777)
+		if err != nil {
+			return -fuse.EIO, ^uint64(0)
+		}
+
+		fd = 100
+		fh.openCount = 1
+		fs.handles[fd] = fh
+		return 0, fd //return a handle that is used by further calls
 	default:
 		return -fuse.ENOENT, ^uint64(0)
 	}
 }
 
 //Getattr returns file attributes
-func (fs *FS) Getattr(path string, stat *fuse.Stat_t, fh uint64) (errc int) {
-	fmt.Println("path", path, "fh(0):", fh == ^uint64(0))
+func (fs *FS) Getattr(path string, stat *fuse.Stat_t, fd uint64) (errc int) {
 	switch path {
 	case "/":
 		stat.Mode = fuse.S_IFDIR | 0555
 		return 0
 	case "/" + filename:
-		stat.Mode = fuse.S_IFREG | 0444
-		stat.Size = int64(len(contents))
+
+		fi, err := os.Stat(filepath.Join(fs.dbdir, path))
+		if err != nil {
+			fmt.Println("stat", err)
+			return -fuse.EIO
+		}
+
+		stat.Mode = fuse.S_IFREG | 0777 //@TODO set this correct perms
+		// stat.Size = int64(len(contents))
+		stat.Size = fi.Size()
 		return 0
 	default:
 		return -fuse.ENOENT
 	}
 }
 
+//Truncate changes the size of a file
+func (fs *FS) Truncate(path string, size int64, fd uint64) int {
+	err := fs.handles[fd].Truncate(size)
+	if err != nil {
+		fmt.Println("failed to truncate")
+		return -fuse.EIO
+	}
+
+	// contents = ""
+	return 0
+}
+
+//Release closes an open file.
+func (fs *FS) Release(path string, fd uint64) int {
+	err := fs.handles[fd].Close()
+	if err != nil {
+		fmt.Println("close", err)
+		return -fuse.EIO
+	}
+	return 0
+}
+
+//Write writes data to a file.
+func (fs *FS) Write(path string, buff []byte, ofst int64, fd uint64) (n int) {
+	var err error
+	n, err = fs.handles[fd].WriteAt(buff, ofst)
+	if err != nil {
+		fmt.Println("write at ", err)
+		return -fuse.EIO
+	}
+
+	return n
+}
+
 //Read file contents
-func (fs *FS) Read(path string, buff []byte, ofst int64, fh uint64) (n int) {
-	endofst := ofst + int64(len(buff))
-	if endofst > int64(len(contents)) {
-		endofst = int64(len(contents))
+func (fs *FS) Read(path string, buff []byte, ofst int64, fd uint64) (n int) {
+	var err error
+	fh := fs.handles[fd]
+	n, err = fh.ReadAt(buff, ofst)
+	if err != nil {
+		fmt.Println("readat", err)
+		return -fuse.EIO
 	}
-	if endofst < ofst {
-		return 0
-	}
-	n = copy(buff, contents[ofst:endofst])
-	return
+
+	return n
 }
 
 //Readdir read a path as a directory
 func (fs *FS) Readdir(path string,
 	fill func(name string, stat *fuse.Stat_t, ofst int64) bool,
 	ofst int64,
-	fh uint64) (errc int) {
+	fd uint64) (errc int) {
+
 	fill(".", nil, 0)
 	fill("..", nil, 0)
 	fill(filename, nil, 0)
--- simple/simple_test.go
+++ simple/simple_test.go
@@ -2,6 +2,7 @@ package simple_test
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -12,28 +13,60 @@ import (
 	"github.com/billziss-gh/cgofuse/fuse"
 )
 
+func WriteFile(filename string, data []byte, perm os.FileMode) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+	if err != nil {
+		return err
+	}
+	n, err := f.Write(data)
+	if err == nil && n < len(data) {
+		err = io.ErrShortWrite
+	}
+	if err1 := f.Close(); err == nil {
+		err = err1
+	}
+	return err
+}
+
 func TestReadingFIles(t *testing.T) {
-	dir, err := ioutil.TempDir("", "dfs_")
+	dbdir, err := ioutil.TempDir("", "dfs_")
+	ok(t, err)
+	err = ioutil.WriteFile(filepath.Join(dbdir, "hello"), []byte("hello, world\n"), 0777)
 	ok(t, err)
 
-	_ = dir
+	defer func() {
+		data, err := ioutil.ReadFile(filepath.Join(dbdir, "hello"))
+		ok(t, err)
+		equals(t, "hello, fuse\n", string(data))
+	}()
 
 	t.Run("fs_1", func(t *testing.T) {
-		fs := simple.NewFS()
+		fs := simple.NewFS(dbdir)
 		host := fuse.NewFileSystemHost(fs)
-		dir := filepath.Join(os.TempDir(), fmt.Sprintf("%d_%s", time.Now().UnixNano(), t.Name()))
+		fsdir := filepath.Join(os.TempDir(), fmt.Sprintf("%d_%s", time.Now().UnixNano(), t.Name()))
 
 		go func() {
-			ok := host.Mount(dir, []string{})
+			ok := host.Mount(fsdir, []string{})
 			equals(t, true, ok)
 		}()
 
 		<-fs.ReadyCh
 
 		t.Run("read file", func(t *testing.T) {
-			data, err := ioutil.ReadFile(filepath.Join(dir, "hello"))
+			data, err := ioutil.ReadFile(filepath.Join(fsdir, "hello"))
 			ok(t, err)
 			equals(t, "hello, world\n", string(data))
+		})
+
+		t.Run("write file", func(t *testing.T) {
+			err := WriteFile(filepath.Join(fsdir, "hello"), []byte("hello, fuse\n"), 0777)
+			ok(t, err)
+		})
+
+		t.Run("read file again", func(t *testing.T) {
+			data, err := ioutil.ReadFile(filepath.Join(fsdir, "hello"))
+			ok(t, err)
+			equals(t, "hello, fuse\n", string(data))
 		})
 
 		t.Run("unmounting", func(t *testing.T) {
