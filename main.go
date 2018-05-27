de6b79702048b35f62b40ff964a7a51c9bf3a76e
advanderveer <advanderveer@gmail.com>
2018-05-27 18:12:21 +0200

    added some windows fixes

--- main.go
+++ main.go
@@ -1,34 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
+	"github.com/advanderveer/dfs/ffs"
+	"github.com/advanderveer/dfs/ffs/blocks"
 	"github.com/advanderveer/dfs/ffs/fsrpc"
+	"github.com/advanderveer/dfs/ffs/nodes"
 	"github.com/advanderveer/dfs/memfs"
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
+	"github.com/apple/foundationdb/bindings/go/src/fdb/directory"
 	"github.com/billziss-gh/cgofuse/fuse"
 )
 
-// func db(ns string) (tr fdb.Transactor, ss directory.DirectorySubspace, f func()) {
-// 	fdb.MustAPIVersion(510)
-// 	db, err := fdb.OpenDefault()
-// 	if err != nil {
-// 		log.Fatal("failed to open database:", err)
-// 	}
-//
-// 	dir, err := directory.CreateOrOpen(db, []string{"fdb-tests", ns}, nil)
-// 	if err != nil {
-// 		log.Fatal("failed to create or open app dir:", err)
-// 	}
-//
-// 	return db, dir, func() {
-// 		_, err := dir.Remove(db, nil)
-// 		if err != nil {
-// 			log.Fatal("failed to remove testing dir:", err)
-// 		}
-// 	}
-// }
+func testdb(ns string) (tr fdb.Transactor, ss directory.DirectorySubspace, f func()) {
+	fdb.MustAPIVersion(510)
+	db, err := fdb.OpenDefault()
+	if err != nil {
+		log.Fatal("failed to open database:", err)
+	}
+
+	dir, err := directory.CreateOrOpen(db, []string{"fdb-tests", ns}, nil)
+	if err != nil {
+		log.Fatal("failed to create or open app dir:", err)
+	}
+
+	return db, dir, func() {
+		_, err := dir.Remove(db, nil)
+		if err != nil {
+			log.Fatal("failed to remove testing dir:", err)
+		}
+	}
+}
 
 func main() {
 	logs := log.New(os.Stderr, "ffs/", log.Lshortfile)
@@ -38,15 +43,8 @@ func main() {
 
 	uid := os.Getuid()
 	gid := os.Getgid()
-	fmt.Println("ENODATA", fuse.ENODATA, "ENOSYS", fuse.ENOSYS, "ENOTSUP", fuse.ENOTSUP, "ENOATTR", fuse.ENOATTR)
-	fmt.Println("fuse.S_IFDIR", fuse.S_IFDIR, "fuse.S_IFMT", fuse.S_IFMT)
 	logs.Printf("mounting filesystem from '%s' at '%s' (uid: %d, gid: %d)", os.Args[1], os.Args[2], uid, gid)
 	defer logs.Printf("unmounted, done!")
-
-	// conn, err := net.DialTimeout("tcp", os.Args[1], time.Second*2)
-	// if err != nil {
-	// 	logs.Fatalf("failed to dial: %v", err)
-	// }
 
 	var (
 		fs  fuse.FileSystemInterface
@@ -54,26 +52,26 @@ func main() {
 	)
 
 	switch os.Args[1] {
-	// case "local":
-	// 	logs.Println("using a own-mounted fs")
-	// 	tpdir, err := ioutil.TempDir("", "ffs_")
-	// 	if err != nil {
-	// 		logs.Fatalf("failed to creat temp dir: %v", err)
-	// 	}
-	//
-	// 	db, dir, clean := db(tpdir)
-	// 	defer clean()
-	//
-	// 	bstore, err := blocks.NewStore(tpdir, "blocks")
-	// 	if err != nil {
-	// 		logs.Fatalf("failed to create block store: %v", err)
-	// 	}
-	//
-	// 	defer bstore.Close()
-	// 	fs, err = ffs.NewFS(nodes.NewStore(db, dir), bstore)
-	// 	if err != nil {
-	// 		logs.Fatalf("failed to create filesystem: %v", err)
-	// 	}
+	case "local":
+		logs.Println("using a own-mounted fs")
+		tpdir, err := ioutil.TempDir("", "ffs_")
+		if err != nil {
+			logs.Fatalf("failed to creat temp dir: %v", err)
+		}
+
+		db, dir, clean := testdb(tpdir)
+		defer clean()
+
+		bstore, err := blocks.NewStore(tpdir, "blocks")
+		if err != nil {
+			logs.Fatalf("failed to create block store: %v", err)
+		}
+
+		defer bstore.Close()
+		fs, err = ffs.NewFS(nodes.NewStore(db, dir), bstore)
+		if err != nil {
+			logs.Fatalf("failed to create filesystem: %v", err)
+		}
 	case "memfs":
 		logs.Println("using a memory fs")
 		fs = memfs.NewMemfs()
